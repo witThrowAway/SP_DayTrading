bb65830666e557a9f1228c19790ec4ef3388dfbf
Ryan Gould <ryan.gould.soccer@gmail.com>
2020-05-15 15:23:44 -0400

    OOP in python is lame

--- feed.py
+++ feed.py
@@ -5,25 +5,34 @@ import sys
 
 import backtrader as bt
 import backtrader.feeds as btfeeds
+import pandas as pd
+import redditScrape as rs
 
 class TestStrategy(bt.Strategy):
+    scraper = 0 #???? make this param a redditScrape object??????
     def log(self, txt, dt=None):
         ''' Logging function for this strategy'''
         dt = dt or self.datas[0].datetime.date(0)
         print('%s, %s' % (dt.isoformat(), txt))
 
-    def __init__(self):
+    def __init__(self, scraper):
         # Keep a reference to the "close" line in the data[0] dataseries
+        self.scraper = scraper
         self.dataclose = self.datas[0].close
 
     def next(self):
         # Simply log the closing price of the series from the reference
         self.log('Close, %.2f' % self.dataclose[0])
-
-        #if the closing prcie is greater than the previous
-        if self.dataclose[0] > self.dataclose[-1]:
-            self.log("BUY BUY BUY")
-            self.buy()
+        df = pd.read_csv("/Users/ryangould/Downloads/SP_DayTrading/datafeed.csv")
+        hammertimes = self.scraper.areThereBearishHammerTimes(df)
+        simpleMovingAverage = self.scraper.simpleMovingAverage(-5,0,self.data)
+        y = 0
+        for x in hammertimes:
+            y = y + 1
+            #if the closing prcie is greater than the previous
+            if self.dataclose[0] == hammertimes[y] and simpleMovingAverage < 0:
+                self.log("BUY BUY BUY")
+                self.buy()
 
 
 if __name__ == '__main__':
@@ -48,7 +57,9 @@ if __name__ == '__main__':
     cerebro = bt.Cerebro()
     cerebro.broker.setcash(1000.0)
     cerebro.adddata(data)
-    cerebro.addstrategy(TestStrategy)
+    scraper = rs.Scraper()
+    strategy = TestStrategy(scraper)
+    cerebro.addstrategy(strategy)
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
     cerebro.run()
     print('final value: %.2f' % cerebro.broker.getvalue())
--- redditScrape.py
+++ redditScrape.py
@@ -6,7 +6,7 @@ import pandas as pd
 import mplfinance as mpf
 
 
-ALPHAVANTAGE_API_KEY = ['4V135H1E1WD8KL2H','4V135H1E1WD8KL2H''1QHMI47XYRQ7TWAO','9FMDAA394M57MTO4','TUN4UO2WBJB6EOP8','I0QTTDE58ZSTDYBV','1TV5Q263WGF2U2WA']
+ALPHAVANTAGE_API_KEY = ['4V135H1E1WD8KL2H','4V135H1E1WD8KL2H','1QHMI47XYRQ7TWAO','9FMDAA394M57MTO4','TUN4UO2WBJB6EOP8','I0QTTDE58ZSTDYBV','1TV5Q263WGF2U2WA']
 
 
 class Scraper():
@@ -40,25 +40,48 @@ class Scraper():
         SMAplot = mpf.make_addplot(sma_data[-50:]['SMA'])
         mpf.plot(data[-50:], type='candlestick', volume=True, title='Last 25 Minutes of ' + top_stock, addplot=SMAplot)
     
-    def areThereBullishHammerTimes(self, data):
-        hammerTimesSubSet = data[data['Open'] >= data['High'] - (data['High'] * (1 / 200))]
+    def areThereBearishHammerTimes(self, data):
+        hammerTimesSubSet = data[data['Open'] <= data['High'] - (data['High'] * (1 / 200))]
         hammerTimesSubSet1 = hammerTimesSubSet[hammerTimesSubSet['Open'] > hammerTimesSubSet['Close']]
-        final = hammerTimesSubSet1[hammerTimesSubSet1['Low'] * 1.01 < hammerTimesSubSet1['Close']]
+        final = hammerTimesSubSet1[hammerTimesSubSet1['Low'] * 1.025 < hammerTimesSubSet1['Close']]
         return final
+    def simpleMovingAverage(self, startingIndex, endIndex, data):
+        simpleMovingAverage = 0.0
+        sum = 0.0
+        divisor = endIndex - startingIndex
+        y = startingIndex
+        for x in data:
+            y += 1
+            sum += data.iat[y, 1]
+            if y == endIndex:
+                break
+        simpleMovingAverage = sum / divisor
+        if (data.iat[startingIndex,1] > data.iat[endIndex,1]):
+            simpleMovingAverage = -simpleMovingAverage
+
+        return simpleMovingAverage
     
 if __name__ == '__main__':
     scraper = Scraper()
-    unscreened_stocks = scraper.unscreenedStocks()
-    topStock = scraper.topStock(unscreened_stocks)
-    for x in unscreened_stocks:
+    #unscreened_stocks = scraper.unscreenedStocks()
+   # topStock = scraper.topStock(unscreened_stocks)
+    '''  for x in unscreened_stocks:
         #print(x)
         realTimeData = scraper.getRealTimeDataForSingleStock(x[0])
         hammertimes = scraper.areThereBullishHammerTimes(realTimeData)
         print(hammertimes)
-        
-    hammertimes = scraper.areThereBullishHammerTimes(realTimeData)
+    '''
+    df = pd.read_csv("/Users/ryangould/Downloads/SP_DayTrading/datafeed.csv")
+    hammertimes = scraper.areThereBearishHammerTimes(df)
+    simpleMovingAverage = scraper.simpleMovingAverage(379, 382, df)
     print(hammertimes)
-    technicalIndicators = scraper.technicalIndicators(realTimeData,topStock)
+    print(simpleMovingAverage)
+    print(df.iat[0,0])
+    print(df.iat[379,1])
+    print(df.iat[380,1])
+    print(df.iat[381,1])
+    print(df.iat[382,1])
+   # technicalIndicators = scraper.technicalIndicators(df,topStock)
     
     #scraper.plot(realTimeData,technicalIndicators,topStock)
     
