d5aedbe3723fc6b7b320cd505dfe86b0e4a7a77c
Ryan Gould <ryan.gould.soccer@gmail.com>
2020-05-26 14:07:00 -0400

    dbConnector v1

--- alpacaDrip.py
+++ alpacaDrip.py
@@ -1,94 +1,126 @@
-import alpaca_trade_api as tradeapi
+#!/usr/bin/python
+import alpaca_trade_api as api
 import datetime
 import pandas as pd
+import pymysql.cursors
+import pymysql
 
 BASE_URL = 'https://paper-api.alpaca.markets'
 KEY_ID = 'PKXJ9PFWUR1PV0W4CR3Z'
 SECRET_KEY = 'fQPmWsENWU7hrWlxoyGrPDsOOxehqkielyVs3bJ8'
 
+class Strategy:
+    def getBar(self, symbol, interval,start):
+        barset = api.get_barset(symbol, interval, limit=1, after=start)
+        mvis_bars = barset['MVIS']
+        return mvis_bars
+    def isHammerBar(self, bar):
+        if bar[0].o <= (bar[0].h - bar[0].h * (1/200)):
+            if bar[0].o > bar[0].c:
+                if bar[0].l * 1.025 < bar[0].c:
+                    return True
+    def simpleMovingAverageAcrossTime(self, symbol, interval,start):
+        bar = api.get_barset(symbol, interval, limit=5, after=start)
+        sum = 0.0
+        simpleMovingAverage = 0.0
+        y = 0
+        n = 0
+        for x in bar:
+            y += 1
+            n += 1.0
+            sum += bar[y].c
+        simpleMovingAverage = sum/n
+        if bar[0].c > bar[3].c  and bar[3].c > bar[5].c:
+            simpleMovingAverage = -simpleMovingAverage
 
-def getBar(symbol, interval,start):
-    barset = api.get_barset(symbol, interval, limit=1, after=start)
-    mvis_bars = barset['MVIS']
-    return mvis_bars
-def isHammerBar(bar):
-    if bar[0].o <= (bar[0].h - bar[0].h * (1/200)):
-        if bar[0].o > bar[0].c:
-            if bar[0].l * 1.025 < bar[0].c:
-                return True
-def simpleMovingAverageAcrossTime(symbol, interval,start):
-    bar = api.get_barset(symbol, interval, limit=5, after=start)
-    sum = 0.0
-    simpleMovingAverage = 0.0
-    y = 0
-    n = 0
-    for x in bar:
-        y += 1
-        n += 1.0
-        sum += bar[y].c
-    simpleMovingAverage = sum/n
-    if bar[0].c > bar[3].c  and bar[3].c > bar[5].c:
-        simpleMovingAverage = -simpleMovingAverage
+        return simpleMovingAverage
 
-    return simpleMovingAverage
+    #function containing a complete strategy
+    def hammerTimeTrading(self, df,symbol):
+        #selectedTime is always a minute behind the present
+        selectedTime = datetime.datetime.now() - datetime.timedelta(hours=0, minutes=1)
+        #selectedWindow is always X minutes before present
+        selectedWindow = datetime.datetime.now() - datetime.timedelta(hours=0, minutes=5)
+        #get last minute info for selected symbol
+        bar = self.getBar(symbol,'1Min',selectedTime)
 
-#function containing a complete strategy
-def hammerTimeTrading(df,symbol):
-    #selectedTime is always a minute behind the present
-    selectedTime = datetime.datetime.now() - datetime.timedelta(hours=0, minutes=1)
-    #selectedWindow is always X minutes before present
-    selectedWindow = datetime.datetime.now() - datetime.timedelta(hours=0, minutes=5)
-    #get last minute info for selected symbol
-    bar = getBar(symbol,'1Min',selectedTime)
+        #####block to verify if cronjob runs######
+        f = open("/Users/ryangould/Downloads/SP_DayTrading/test.txt", "a")
+        f.write(bar)
+        f.close()
+        #This block can be swapped to database write calls later on
+        ##########################################
 
-    #####block to verify if cronjob runs######
-    f = open("/Users/ryangould/Downloads/SP_DayTrading/test.txt", "a")
-    f.write(bar)
-    f.close()
-    #This block can be swapped to database write calls later on
-    ##########################################
+        #parse out data to add to dataframe
+        data = {'Time':[bar[0].t], 'Open':[bar[0].o], 'High':[bar[0].h], 'Low':[bar[0].l], 'Close':[bar[0].c], 'Volume':[bar[0].v]}
+        df = df.append(data, ignore_index=True)
+        #unused df - need to write to external storage so simpleMovingAverage can read from it
+        #to reduce overall API calls
 
-    #parse out data to add to dataframe
-    data = {'Time':[bar[0].t], 'Open':[bar[0].o], 'High':[bar[0].h], 'Low':[bar[0].l], 'Close':[bar[0].c], 'Volume':[bar[0].v]}
-    df = df.append(data, ignore_index=True)
-    #unused df - need to write to external storage so simpleMovingAverage can read from it
-    #to reduce overall API calls
-
-    #check if the current bar is a hammer and the last 5 bars were a negative moving average
-    if isHammerBar(bar) and simpleMovingAverageAcrossTime(symbol,'1Min',selectedWindow) < 0:
-        #buy position at hammer (current bar)
-        api.submit_order(
-            symbol=symbol,
-            side='buy',
-            type='market',
-            qty='100',
-            time_in_force='day',
-            order_class='bracket',
-            take_profit=dict(
-                limit_price=bar[0].c + .5,
-            ),
-            stop_loss=dict(
-                stop_price=bar[0].c - .5,
+        #check if the current bar is a hammer and the last 5 bars were a negative moving average
+        if self.isHammerBar(bar) and self.simpleMovingAverageAcrossTime(symbol,'1Min',selectedWindow) < 0:
+            #buy position at hammer (current bar)
+            api.submit_order(
+                symbol=symbol,
+                side='buy',
+                type='market',
+                qty='100',
+                time_in_force='day',
+                order_class='bracket',
+                take_profit=dict(
+                    limit_price=bar[0].c + .5,
+                ),
+                stop_loss=dict(
+                    stop_price=bar[0].c - .5,
+                )
             )
-        )
-        print('ORDER SUBMITTED')
+            print('ORDER SUBMITTED')
 
 if __name__ == '__main__':
 
-    api = tradeapi.REST(KEY_ID, SECRET_KEY, BASE_URL)
+    api = api.REST(KEY_ID, SECRET_KEY, BASE_URL)
 
     account = api.get_account()
-    print(account.cash)
+
+
+    # Connect to the database
+    connection = pymysql.connect(host='localhost',
+                             user='root',
+                             #password='admin',
+                             db='GrandExchange',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+
+            sql = "INSERT INTO `Stonks` (`symbol`, `high`) VALUES (%s, %s)"
+            cursor.execute(sql, ('HUGEDICK', 1.69))
+
+        # connection is not autocommit by default. So you must commit to save
+        # your changes.
+        connection.commit()
+
+        with connection.cursor() as cursor:
+            # Read a single record
+            sql = "SELECT `id`, `symbol` FROM `Stonks` WHERE `symbol`=%s"
+            cursor.execute(sql, ('HUGEDICK',))
+            result = cursor.fetchone()
+            print(result)
+    finally:
+        connection.close()
+    #print(account.cash)
 
     #bar = getBar('MVIS','1Min',selectedTime)
-    df = pd.DataFrame(columns=['Time','Open','High','Low','Close','Volume'])
+    #df = pd.DataFrame(columns=['Time','Open','High','Low','Close','Volume'])
     #data = {'Time':[bar[0].t], 'Open':[bar[0].o], 'High':[bar[0].h], 'Low':[bar[0].l], 'Close':[bar[0].c], 'Volume':[bar[0].v]}
     #df = df.append(data,ignore_index=True)
 
     # df.append(getBar('MVIS','1Min',selectedTime))
     #hammerTimeTrading strategy is left uncalled until cronjob works
     #hammerTimeTrading(df,'MVIS')
-    print(df)
-    f = open("/Users/ryangould/Downloads/SP_DayTrading/test.txt", "a")
-    f.write(account.cash)
-    f.close()
+    #print(df)
+    #f = open("/Users/ryangould/Downloads/SP_DayTrading/test.txt", "a")
+    #f.write(account.cash)
+    #f.close()
