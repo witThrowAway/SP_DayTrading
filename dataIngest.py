c8f6734fb6b718a5ea8215b447fa9f84f2f53f43
Ryan Gould <ryan.gould.soccer@gmail.com>
2020-06-02 14:48:54 -0400

    comments

--- dataIngest.py
+++ dataIngest.py
@@ -29,9 +29,8 @@ if __name__ == '__main__':
     for i in symbols:
         bar = api.get_barset(symbols[count], '1Min', limit=1, after=selectedTime)
         barset = bar[symbols[count]]
-        #print(barset)
-        #print(type(barset))
-        #id - symbol - high - low - open - close - volume - shareCount - timestamp - barType
+        #check if barset has a value to account for API response time
         if barset != None and barset:
+            #symbol - high - low - open - close - volume - shareCount - timestamp - barType
             connector.insertBar(symbols[count], barset[0].h, barset[0].l, barset[0].o, barset[0].c, barset[0].v, 1, 'barType', connection)
         count += 1
