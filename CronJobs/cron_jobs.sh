dd3af3a788b8d72ea4479ceadb89fcfcba882f65
Ryan Gould <ryan.gould.soccer@gmail.com>
2020-06-10 16:00:41 -0400

    cronjob fix

--- CronJobs/cron_jobs.sh
+++ CronJobs/cron_jobs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/python3.8
-./dataIngest.py
-./Strategies/strategyExecuter.py
+/usr/bin/python3.8 /home/trade/Desktop/SP_DayTrading/dataIngest.py
+/usr/bin/python3.8 /home/trade/Desktop/SP_DayTrading/Strategies/strategyExecuter.py
 
