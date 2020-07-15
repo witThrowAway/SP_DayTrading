0d74d36120c5ecc60d60595c3a1db9c86a5455bb
Ryan Gould <ryan.gould.soccer@gmail.com>
2020-07-15 12:55:07 -0400

    api call swap

--- dataIngest.py
+++ dataIngest.py
@@ -2,6 +2,7 @@ import dbConnector as db
 import alpaca_trade_api as tradeapi
 from Scrapers import redditScraper as rs
 import datetime
+from datetime import time
 import alpacaDrip as ad
 import pandas as pd
 from numpy import float as floaty
@@ -10,11 +11,12 @@ from numpy import float as floaty
 if __name__ == '__main__':
 
     start = datetime.datetime.now()
+
     if datetime.datetime.now().time() > datetime.time(9,30):
         #initialize API information
         ALPACA_KEY_ID = 'PK3VZLXGJAE5FPVLWCOU'
         ALPACA_SECRET_KEY = r'NtLnmeY6PtUpPXD2kGblhezLg/6f4lHqEcIqrR/3'
-
+        APCA_RETRY_MAX=1
         api = tradeapi.REST(
             key_id=ALPACA_KEY_ID,
             secret_key=ALPACA_SECRET_KEY,
@@ -26,30 +28,27 @@ if __name__ == '__main__':
         connection = connector.createConnection()
 
         #Create time window to make api call for
-        now = datetime.datetime.now()
         window = datetime.datetime.now() - datetime.timedelta(minutes=1)
-
         unscreened_stocks = connector.getMentions(connection)
         count = 0
         barType = 'barType'
         strategy = ad.Strategy()
         # iterate through symbols getting bar info for each symbol of last minute
-        for x in unscreened_stocks:
-            # bar = api.get_barset(symbols[count], '1Min', limit=1, after=selectedTime)
+        for x in unscreened_stocks[0:65]:
             df = pd.DataFrame()
             tryCounter = 0
             # check if df has a value to account for API response time
             while df.empty and tryCounter < 10:
-                df = api.polygon.historic_agg_v2(unscreened_stocks[count]["symbol"], 1, 'minute', limit=1,
-                                             _from=str(window), to=str(now)).df
+                df = api.get_barset(unscreened_stocks[count]["symbol"], '1Min', limit=1, after=window).df
                 tryCounter +=1
             count += 1
             if not df.empty:
-                if strategy.isHammerBar(df):
-                    barType = 'hammer'
                 # symbol - high - low - open - close - volume - shareCount - timestamp - barType
                 try:
-                    connector.insertBar(unscreened_stocks[count]["symbol"], floaty(df['high']), floaty(df['low']), floaty(df['open']),
-                                floaty(df['close']), floaty(df['volume']), 1, barType, connection)
+                    symbol = unscreened_stocks[count]["symbol"]
+                    connector.insertBar(symbol, floaty(df[symbol]['high'][0]), floaty(df[symbol]['low'][0]), floaty(df[symbol]['open'][0]),
+                                floaty(df[symbol]['close'][0]), floaty(df[symbol]['volume'][0]), 1, barType, connection)
                 except Exception as e:
                     print(str(e))
+
+    print("--- %s seconds ---" % (datetime.datetime.now() - start))
